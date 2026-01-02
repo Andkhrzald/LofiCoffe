867f7ce3e40a8c37d105ebc2e43b8ade104b6680
Andkhrzald <Maxcyandkh20@gmail.com>
2026-01-03 03:11:38 +0700

    Style Container Login

--- lib/main.dart
+++ lib/main.dart
@@ -10,122 +10,147 @@ class loginPage extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            //id=kotak
-            color: const Color.fromARGB(255, 255, 245, 242),
-            width: double.infinity,
-            child: new Column(
-              mainAxisAlignment:
-                  MainAxisAlignment.start, //nempel ke atas tulisanya
-              children: [
-                SizedBox(height: 40), //Jarak dari atas
-                Text(
-                  "Lo-fi Coffee",
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
-
-                SizedBox(height: 0), //Jarak dari atas ke gambar WanitaLofi
-
-                Image.asset(
-                  'assets/image/WanitaLoFi.png',
-                  width: 250,
-                  fit: BoxFit.contain,
-                ),
-
-                SizedBox(height: 1), // jarak kotak Login
-
-                Container(
-                  width: 600,
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  padding: EdgeInsets.all(30),
-                  decoration: BoxDecoration(
-                    color: Colors.brown[800],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                      bottomLeft: Radius.circular(0), //Tetap Lancip yang kiri
-                      bottomRight: Radius.circular(0), //Kanan Tetap Lancip
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              color: const Color.fromARGB(255, 255, 245, 242),
+              width: double.infinity,
+              height: constraints.maxHeight, // ⬅️ full tinggi layar
+              child: Column(
+                children: [
+                  const SizedBox(height: 40),
+                  const Text(
+                    "Lo-fi Coffee",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown,
                     ),
                   ),
 
-                  child: Column(
-                    children: [
-                      Text(
-                        "Tunggu Sejenak",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                  const SizedBox(height: 20),
+
+                  Expanded(
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        // GAMBAR WANITA
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Image.asset(
+                            'assets/image/WanitaLoFi.png',
+                            height: 350,
+                            fit: BoxFit.contain,
+                          ),
                         ),
-                      ),
 
-                      SizedBox(height: 12), // jarak Text Filosofi Kopi
-                      Text(
-                        "Tak perlu berlebihan, satu cangkir kopi sudah cukup untuk menemani jiwa yang mencari ketenangan. Dalam Hangatnya aroma yang perlahan mengusir ruang.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 13, color: Colors.white70),
-                      ),
-
-                      SizedBox(height: 20), // jarak batton Login
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          OutlinedButton(
-                            onPressed: () {},
-                            style: OutlinedButton.styleFrom(
-                              side: BorderSide(color: Colors.white),
-                            ),
-                            child: Text(
-                              "Login",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-
-                          OutlinedButton(
-                            onPressed: () {},
-                            style: OutlinedButton.styleFrom(
-                              side: BorderSide(color: Colors.white),
-                            ),
-                            child: Text(
-                              "Register",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      SizedBox(height: 60), // jarak Text Tamu
-
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "Introvert?",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
+                        // KOTAK COKLAT (MENYENTUH BAWAH)
+                        Positioned(
+                          left: 10,
+                          right: 10,
+                          bottom: 0, // ✅ NEMPEL KE BAWAH LAYAR
+                          child: Container(
+                            height:
+                                constraints.maxHeight *
+                                0.4, // ⬅️ INI KUNCINYA buat niban ke atas
+                            padding: const EdgeInsets.all(30),
+                            decoration: BoxDecoration(
+                              color: Colors.brown[800],
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(25),
+                                topRight: Radius.circular(25),
                               ),
                             ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Text(
+                                  "Tunggu Sejenak",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
 
-                            TextSpan(
-                              text: " Tamu",
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
+                                const SizedBox(height: 12),
+
+                                const Text(
+                                  "Tak perlu berlebihan, satu cangkir kopi sudah cukup untuk menemani jiwa yang mencari ketenangan. Dalam Hangatnya aroma yang perlahan mengusir ruang.",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+
+                                const SizedBox(height: 20),
+
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    OutlinedButton(
+                                      onPressed: () {},
+                                      style: OutlinedButton.styleFrom(
+                                        side: const BorderSide(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      child: const Text(
+                                        "Login",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    OutlinedButton(
+                                      onPressed: () {},
+                                      style: OutlinedButton.styleFrom(
+                                        side: const BorderSide(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      child: const Text(
+                                        "Register",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                const SizedBox(height: 40),
+
+                                RichText(
+                                  text: const TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: "Introvert?",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: " Tamu",
+                                        style: TextStyle(
+                                          color: Colors.blue,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
